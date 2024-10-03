51ea50bc91086d315d86e2f2e870dbb1acc0317a
Filipe Zaidan <felipezaidan10@gmail.com>
2024-10-03 14:06:49 -0300

    feat: use validation file on form route

--- routes/form.js
+++ routes/form.js
@@ -1,71 +1,16 @@
 var express = require("express");
 var router = express.Router();
+var { validateFormInput } = require("../utils/validation");
 
 router.post("/", function (req, res) {
-  const {
-    studentName,
-    day,
-    month,
-    year,
-    motherName,
-    fatherName,
-    phone,
-    email,
-    grade,
-    shift,
-    activities,
-  } = req.body;
-
-  let errors = [];
-
-  if (
-    !studentName ||
-    !day ||
-    !month ||
-    !year ||
-    !motherName ||
-    !fatherName ||
-    !phone ||
-    !email ||
-    !grade ||
-    !shift
-  ) {
-    errors.push({ msg: "Por favor, preencha todos os campos obrigatórios." });
-  }
-
-  if (
-    isNaN(day) ||
-    isNaN(month) ||
-    isNaN(year) ||
-    day < 1 ||
-    day > 31 ||
-    month < 1 ||
-    month > 12 ||
-    year > 1900 ||
-    year <= new Date().getFullYear()
-  ) {
-    errors.push({ msg: "Data de nascimento inválida." });
-  }
-
-  if (!email.includes("@")) {
-    errors.push({ msg: "E-mail inválido." });
-  }
-
-  const validDDDs = [11, 21, 31, 41, 51, 61, 71, 81, 91];
-  let ddd = parseInt(phone.substring(0, 2));
-  if (!validDDDs.includes(ddd)) {
-    errors.push({ msg: "DDD inválido." });
-  }
-
-  if (activities && activities.length > 3) {
-    errors.push({ msg: "No máximo 3 atividades extracurriculares." });
-  }
+  const errors = validateFormInput(req.body);
 
   if (errors.length > 0) {
     console.log({ errors });
-    res.status(400).render("errorPage", { errors });
-    res.send("Formulário recebido com sucesso!");
+    return res.status(400).send({ errors });
   }
+
+  return res.status(200).send("Formulário recebido com sucesso!");
 });
 
 module.exports = router;
